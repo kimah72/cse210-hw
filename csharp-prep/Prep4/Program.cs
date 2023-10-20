abeef2324009b84516a33809ef4eecd3e4bccf0a
kimah72 <min23005@byui.edu>
2023-10-20 14:33:06 -0600

    redid prep 4 to understand missed concepts to improve

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,35 +5,32 @@ class Program
     static void Main(string[] args)
     {
         List<int> numbers = new List<int>();
-
+        // Ask user for a series of numbers, and append each one to a list. Stop when they enter 0
         int userNumber = -1;
-
         while (userNumber != 0)
         {
-            Console.WriteLine("Enter a number (0 to quit): ");
-
+            Console.Write("Enter a number (0 to quit) ");
             string userResponse = Console.ReadLine();
             userNumber = int.Parse(userResponse);
-
             if (userNumber != 0)
             {
                 numbers.Add(userNumber);
             }
         }
-
+        // Compute the sum, or total, of the numbers in the list
         int sum = 0;
         foreach (int number in numbers)
         {
             sum += number;
         }
-
         Console.WriteLine($"The sum is: {sum}");
 
-        float average = ((float)sum) /numbers.Count;
-        Console.WriteLine($"The average is: {average}");
+        // Compute the average of the numbers in the list
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average} ");
 
+        // Find the maximum, or largest, number in the list
         int max = numbers[0];
-
         foreach (int number in numbers)
         {
             if (number > max)
@@ -41,7 +38,22 @@ class Program
                 max = number;
             }
         }
-
-        Console.WriteLine($"The max is: {max}");
+        Console.WriteLine($"The largest number is: {max}");
+        // Find the smallest number in the list
+        int min = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number < min)
+            {
+                min = number;
+            }
+        }
+        Console.WriteLine($"The smallest number is: {min}");     
+        // Sort the list from smallest to largest
+        numbers.Sort();
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }

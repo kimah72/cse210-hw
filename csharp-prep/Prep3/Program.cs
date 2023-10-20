7b94bbbe8cf1a27bcb9abaf0f39ae868f43af3d2
kimah72 <min23005@byui.edu>
2023-10-20 13:28:28 -0600

    Redo of the Magic Number prep to understand the concepts I may have missed.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,29 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Start by asking the user for the magic number.
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 101);
-
+        //offsets the value of the integer
         int guess = -1;
+        // making the count start at zero
+        int guessCount = 0;
 
         while (guess != magicNumber)
         {
-            Console.WriteLine("What is your guess?");
+            //Ask the user for a guess
+            Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
-
-            if (magicNumber > guess)
+            // this is the counter for how many guesses it will take to find the Magic Number
+            guessCount++;
+            //Using an if statement, determine if the user needs to guess higher or lower next time, or tell them if they guessed it.
+            
+            if (guess < magicNumber)
             {
                 Console.WriteLine("Higher");
             }
-            else if (magicNumber < guess)
+            else if (guess > magicNumber)
             {
                 Console.WriteLine("Lower");
             }
             else
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine("You guessed the Magic Number!");
             }
-            
-        } 
+
+        }
+        Console.WriteLine($"You made {guessCount} guesses to find the Magic Number.");
     }
 }

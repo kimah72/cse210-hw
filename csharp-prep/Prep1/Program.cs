7e7fb490c361acaafc1669d0d992e71a28df7ef9
kimah72 <min23005@byui.edu>
2023-10-20 11:13:31 -0600

    reviewing all older assignments to understand where I misunderstood concepts.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,15 +1,24 @@
+// system package
 using System;
 
+// shell that contains everything in the program
 class Program
 {
+    // main function tells it where to start. Main is the place!
+    // static = doesn't belong to an instance of a class. It can be run directly
+    // void = tells us what it's going to return when it's done (nothing)
+    // Main = is the name of the function
+    // () states when it starts that it can receive perameters. It can receive an array of strings called args
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
-        string first = Console.ReadLine();
+        // Within the body of these curly braces we can write the code that we want to be run
+        // Everything will reside here as we start writing our code.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
 
-        Console.WriteLine("What is your last name?");
-        string last = Console.ReadLine();
-
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }

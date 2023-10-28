fb9e3c78b38fa59bce19dc1f19753d1f86f13abe
kimah72 <min23005@byui.edu>
2023-10-28 17:57:48 -0600

    cleaned up code

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,10 +1,12 @@
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  public class Scripture
-  {
-    private Reference _reference;
-    private List<Word> _words = new List<Word>();
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private readonly Reference _reference;
+    private readonly List<Word> _words = new List<Word>();
+    private readonly Random _random = new Random(); // Moved the Random object initialization to here
 
     public Scripture(string book, int chapter, int verseStart, int verseEnd, string verse1Text, string verse2Text)
     {
@@ -20,9 +22,8 @@
 
         do
         {
-            var countWord = _words.Count();
-            Random r = new Random();
-            int rIndex = r.Next(0, countWord);
+            int countWord = _words.Count; // Removed unnecessary () from Count
+            int rIndex = _random.Next(0, countWord);
             Word word = _words[rIndex];
             if (word.ShowWord())
             {
@@ -46,4 +47,4 @@
     {
         return _reference;
     }
-  }
+}

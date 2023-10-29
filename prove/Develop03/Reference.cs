dc5026ea2f6f4058326f08149ba97916bf966e66
kimah72 <min23005@byui.edu>
2023-10-28 18:00:57 -0600

    removed repetitive new word classes

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,37 +1,27 @@
-  using System;
-  
-  public class Reference
-  {
+using System;
+
+public class Reference
+{
     private string _book;
     private int _chapter;
     private int _verseStart;
     private int _verseEnd;
 
-    public Reference()
+    public Reference(string book, int chapter, int verseStart, int verseEnd = 0)
     {
-    }
-     public Reference(string book, int chapter, int verseStart)
-     {
-        _book = book;
-        _chapter = chapter;
-        _verseStart = verseStart;
-     }
-
-     public Reference(string book, int chapter, int verseStart, int verseEnd)
-     {
         _book = book;
         _chapter = chapter;
         _verseStart = verseStart;
         _verseEnd = verseEnd;
-     }
+    }
 
-     public void DisplayReference()
-     {
+    public void DisplayReference()
+    {
         Console.Write($"{_book}, {_chapter}:{_verseStart}");
 
         if (_verseEnd > 0)
         {
-          Console.Write($"-{_verseEnd} ");
+            Console.Write($"-{_verseEnd} ");
         }
-     }
-  }
+    }
+}

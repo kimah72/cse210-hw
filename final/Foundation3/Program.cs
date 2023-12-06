0adb8397e4b46371a489a34327d515c133a7e253
kimah72 <min23005@byui.edu>
2023-12-05 20:14:12 -0700

    changed time to military so it would make sense

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -11,7 +11,7 @@ class Program
         // Create Events
         Lecture lectureEvent = new Lecture("Ted Talk", "Explore Programming with Class", DateTime.Now.AddDays(30), new TimeSpan(14, 0, 0), address1, "John Doe", 100);
         Reception receptionEvent = new Reception("Networking Mixer", "Meet and Greet", DateTime.Now.AddDays(80), new TimeSpan(18, 30, 0), address2, "rsvp@example.com");
-        OutdoorGathering outdoorEvent = new OutdoorGathering("Luminaria", "Experience a beautiful, winter light show", DateTime.Now.AddDays(60), new TimeSpan(12, 0, 0), address1, "Dress for cold weather!");
+        OutdoorGathering outdoorEvent = new OutdoorGathering("Luminaria", "Experience a beautiful, winter light show", DateTime.Now.AddDays(60), new TimeSpan(21, 0, 0), address1, "Dress for cold weather!");
 
         // Display marketing messages
         Console.WriteLine("Lecture Event - Standard Details:\n" + lectureEvent.GenerateStandardDetails() + "\n");
